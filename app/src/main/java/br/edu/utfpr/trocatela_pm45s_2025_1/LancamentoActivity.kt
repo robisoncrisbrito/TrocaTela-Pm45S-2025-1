857fef57de68f40faea42afc1d2183e184d31e4e
robisoncrisbrito <robison.brito@gmail.com>
2025-04-24 21:52:44 -0300

    Aplicativo Troca Tela com a tela Lançamento pronta

--- app/src/main/java/br/edu/utfpr/trocatela_pm45s_2025_1/LancamentoActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela_pm45s_2025_1/LancamentoActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.utfpr.trocatela_pm45s_2025_1
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,4 +12,7 @@ class LancamentoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lancamento)
     }
+
+    fun btConfirmarOnClick(view: View) {}
+    fun btListarOnClick(view: View) {}
 }
